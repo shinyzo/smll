6bd262533061ef13b3b0d0b588f1c132705424e3
shinyzo <11742979@qq.com>
2017-10-18 21:00:00 +0800

    V1.0
    1111

---
+++ src/components/header/header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="header">
+      <div class="content-wrapper">
+        <div class="avatar">
+          <img width="64" height="64" :src="seller.avatar" />
+        </div>
+        <div class="content">
+          <div class="title">
+            <span class="brand"></span>
+            <span class="name">{{seller.name}}</span>
+          </div>
+          <div class="description">
+            {{seller.description}} / {{seller.deliveryTime}}分钟送达
+
+          </div>
+          <div v-if="seller.supports" class="support">
+            <span class="icon"></span>
+            <span class="">{{seller.supports[0].description}}</span>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="bulletin-wrapper">
+
+      </div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+      props: {
+        seller: {
+          type: Object
+        }
+      }
+    };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
